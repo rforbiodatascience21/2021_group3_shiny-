c96c0cad0c1c64c3e953e816f3f620341643743f
HCEhlers <holgerehlers@hotmail.com>
2021-04-15 09:14:27 +0200

    updating

--- app.R
+++ app.R
@@ -10,6 +10,7 @@
 library(shiny)
 library(shinythemes)
 library(tidyverse)
+library(ggpubr)
 # loading gravier data 
 setwd("~/Desktop/Universitet/DTU/Bioinformatik_og_System_Biologi/1.Semester/R_for_BioScience/2021_group3_shiny-")
 gravier_data <- read_csv("cancer_data_gravier.csv")
@@ -24,15 +25,20 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                                                  selectInput("gene2",
                                                              "Gene 2",
                                                              choices=colnames(gravier_data)),
-                                                 hr(),
                                                  helpText("Select two genes and get the pairwise
-                                                          correlation.")
-                                                 
-                ),
-                plotOutput("scatterplot")
-                ),
+                                                          correlation.")),
+                plotOutput("scatterplot")),
                 
-                tabPanel("PCA", "PCA-plot"),
+                tabPanel("Plot", "Plot",
+                         sidebarPanel(selectInput("gene3",
+                                                  "Gene: ",
+                                                  choices=colnames(gravier_data)),
+                                      selectInput("patient",
+                                                  "Patient ID",
+                                                  choices=rownames(gravier_data))),
+                         plotOutput("hist_plot")),
+                
+                
                 tabPanel("About", "Description:
                          Gravier et al. (2010) have considered small, invasive ductal
                          carcinomas without axillary lymph node involvement (T1T2N0)
@@ -55,7 +61,15 @@ server <- function(input, output) {
       theme_minimal() +
       theme(plot.title = element_text(size = 24))+ 
       labs(title = "Pairwise coreleation between expression levels of two genes")
+      plot1 + stat_cor(method = "pearson", label.x = 3, label.y = 30)
     plot(plot1) 
+    })
+  output$hist_plot <- renderPlot({
+    plot2 <- ggplot(data = gravier_data,
+                    mapping = aes_string(x = input$"patient",
+                                  y = input$"gene3")) +
+                      geom_point()
+    plot(plot2) 
     })
 }
 
